9980cbb750c52b2acc99a260c44baf30ed704e65
José Pach <Paching12@icloud.com>
2023-01-12 00:13:30 -0600

    Rendering project list

--- src/app.ts
+++ src/app.ts
@@ -60,6 +60,40 @@ function Autobind(_: any, _1: string | Symbol, descriptor: PropertyDescriptor) {
 // User input interface
 type IUserInputs = [string, string, number];
 
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: "active" | "finished") {
+    this.templateElement = <HTMLTemplateElement>(
+      document.getElementById("project-list")!
+    );
+    this.hostElement = document.getElementById("app") as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-project-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent =
+      this.type.toUpperCase() + " PROJECT";
+  }
+
+  // Attach to the DOM
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -161,3 +195,5 @@ class ProjectInput {
 }
 
 const inputForm = new ProjectInput();
+const activeProjectList = new ProjectList("active");
+const finishedProjectList = new ProjectList("finished");

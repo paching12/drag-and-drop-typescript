ca6ee1a12dcd4246347dc97a3d89cfc335fcb8c7
José Pach <Paching12@icloud.com>
2023-02-08 22:55:18 -0600

    Declare as a last resort

--- src/app.ts
+++ src/app.ts
@@ -6,4 +6,9 @@ new ProjectInput();
 new ProjectList("active");
 new ProjectList("finished");
 
+declare var GLOBAL: any;
+
 console.log(_.shuffle([1, 2, 3]));
+
+// use global vars
+console.log("GLOBAL", GLOBAL);

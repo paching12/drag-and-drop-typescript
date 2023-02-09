283e7bbd40b9bf46a2c8d6b017e11088efe41da5
José Pach <Paching12@icloud.com>
2023-02-08 22:56:04 -0600

    Rollback changes

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,6 @@
 import { ProjectInput } from "./components/project-input";
 import { ProjectList } from "./components/project-list";
-import _ from "lodash";
 
 new ProjectInput();
 new ProjectList("active");
 new ProjectList("finished");
-
-declare var GLOBAL: any;
-
-console.log(_.shuffle([1, 2, 3]));
-
-// use global vars
-console.log("GLOBAL", GLOBAL);
